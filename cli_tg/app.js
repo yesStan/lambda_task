763b5edff678bb0c47b6a1b59277c2710a2dcc39
yesStan <getiffive@gmail.com>
2022-06-30 15:44:33 +0300

    2

--- cli_tg/app.js
+++ cli_tg/app.js
@@ -1,32 +1,28 @@
 // const TelegramBot = require('node-telegram-bot-api');
 
+// import command from './commands.js';
+import botCommand from './commands.js';
 import TelegramBot from 'node-telegram-bot-api';
 
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'Y5553083920:AAEf8j_0bnTGgh0M_IDVjQzPzzWFXpFxFfA';
+const token = '5553083920:AAEf8j_0bnTGgh0M_IDVjQzPzzWFXpFxFfA';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+bot.setMyCommands(botCommand);
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+    const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
+    console.log(msg);
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+})
+
+// bot.on('start', (welcomeMsg) => {
+//     const chatId = welcomeMsg.chat.id;
+//     bot.sendMessage(chatId, 'hello people')
+// })
+// bot.onText(/\/start/, (msg) => {
+//     bot.sendMessage(msg.chat.id, "Welcome");
+// });
---
+++ cli_tg/commands.js
@@ -0,0 +1,12 @@
+const botCommand = [
+    {
+        command: 'start',
+        description: 'start bot',
+    },
+    {
+        command: 'help',
+        description: 'show available commands',
+    }
+]
+
+export default botCommand;
