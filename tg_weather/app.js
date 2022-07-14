0c07978c17a02a70ecb9c01aa8abc5d13ce82687
yesStan <getiffive@gmail.com>
2022-07-14 21:34:27 +0300

    add telegram menu

--- tg_weather/app.js
+++ tg_weather/app.js
@@ -4,6 +4,7 @@ require('dayjs/locale/ru')
 dayjs.locale('ru')
 const TelegramBot = require('node-telegram-bot-api');
 const ru = require('dayjs/locale/ru');
+const { Command } = require('commander');
 const axios = require('axios').default;
 
 const token = '5553083920:AAEf8j_0bnTGgh0M_IDVjQzPzzWFXpFxFfA'
@@ -15,11 +16,53 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text;
 
-    bot.sendMessage(chatId, ` You wrote: ${msg.text}`);
-    console.log(`${msg.chat.username} wrote text: ${msg.text}`)
+    bot.onText(/^\/start$/, function (msg) {
+        let opts = {
+            reply_to_message_id: msg.message_id,
+            reply_markup: {
+                resize_keyboard: true,
+                keyboard: [['Kiev'], ['Dnipro']]
+            }
+        };
+        bot.sendMessage(msg.chat.id, "choose from the menu", opts);
 
-    if (msg.text) {
+
+    });
+
+    console.log(text);
+
+    // const choose = ((city) => {
+    //     const opts = {
+    //         reply_markup: {
+    //             keyboard: [['3h'], ['6h']]
+    //         }
+    //     };
+    //     return city === 'Kiev' || city === 'Dnipro'
+    //         ? bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
+    //         : bot.sendMessage(msg.chat.id, "in process")
+    // })
+    // choose(text)
+
+    if (text === 'Kiev') {
+        const opts = {
+            reply_markup: {
+                keyboard: [['3h'], ['6h']]
+            }
+        };
+        bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
+    } else if (text === 'Dnipro') {
+        const opts = {
+            reply_markup: {
+                keyboard: [['3h'], ['6h']]
+            }
+        };
+        bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
+    } 
+
+    if (msg.text === '3h') {
+        // bot.sendMessage(chatId, `Погода в: ${msg.text}`);
         try {
             const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=50.4501&lon=30.5234&lang=ru&appid=4468e661cae3911dc87cc649a402ebf1&units=metric`);
             const { list } = data;
@@ -67,7 +110,6 @@ bot.on('message', async (msg) => {
                     ]
                 }
             }, {})
-
 
             const outputData = Object.entries(preparedData).map(([key, value]) => {
                 const title = dayjs(key).format('dddd, D MMMM')
